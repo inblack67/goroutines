b6a35d6f205018ef5578ffe55e318657a960d1b5
inblack67 <inblack67>
2025-06-23 00:31:28 +0530

    aggreating data via channel and waitgroup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/inblack67/goroutines
+
+go 1.24.4
---
+++ sync.go
@@ -0,0 +1,10 @@
+package main
+
+// func Sync() {
+// 	start := time.Now()
+// 	username := fetchUser()
+// 	likes := fetchUserLikes(username)
+// 	match := fetchUserMatch(username)
+// 	fmt.Printf("userName:%s likes:%d match:%s \n", username, likes, match)
+// 	fmt.Println("took:", time.Since(start))
+// }
